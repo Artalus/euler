bd614f79570c40d65678b4a54729c661fb6f95ba
Artalus <artalus-mail@yandex.ru>
2017-08-29 09:55:12 +0300

    basic implmentation for #1 problem

--- src/1.cpp
+++ src/1.cpp
@@ -1,5 +1,6 @@
 #include "euler.h"
 #include <iostream>
+#include <vector>
 
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
@@ -7,8 +8,27 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+using namespace std;
+
+vector<unsigned> get_multiples_less(unsigned limit) {
+	vector<unsigned> result;
+	result.reserve(limit/2);
+
+	for (unsigned i = 1; i < limit; ++i) {
+		if (!(i%3) || !(i%5)) {
+			result.emplace_back(i);
+		}
+	}
+
+	return result;
+}
+
 REGISTER_EULER(1)
 
-	std::cout << "starting problem #1";
+	cout << "starting problem #1: list of natural multiples of 3 or 5\n";
+	for (auto v : get_multiples_less(10)) {
+		cout << v << ' ';
+	}
+	cout << '\n';
 
 REGISTER_EULER_END
